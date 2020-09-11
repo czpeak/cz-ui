4825be9d83938e3135a3edbd5d2bf68b7ca2acf5
czpeak <727164732@qq.com>
2020-09-11 13:45:44 +0800

    11

--- my-plugins/examples/main.js
+++ my-plugins/examples/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-// 导入组件库
-import ColorPicker from './../packages/index'
-// 注册组件库
-Vue.use(ColorPicker)
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
